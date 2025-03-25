ce3645ed79d7f5052eb98b29484411365a0db9a7
venkatsandul1201 <144527803+venkatsandul1201@users.noreply.github.com>
2025-03-25 12:19:48 +0530

    Create Toggle.cpp

---
+++ Toggle.cpp
@@ -0,0 +1,16 @@
+const int ledPin1 = 13;
+const int ledPin2 = 12;
+
+void setup() {
+  pinMode(ledPin1, OUTPUT);
+  pinMode(ledPin2, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(ledPin1, HIGH);
+  digitalWrite(ledPin2, LOW);
+  delay(1000);
+  digitalWrite(ledPin1, LOW);
+  digitalWrite(ledPin2, HIGH);
+  delay(1000);
+}
